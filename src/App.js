e85a382ea31141bc78c30dd91603da858b69950f
jesuskata <rzarate.alex@gmail.com>
2018-10-19 23:16:42 -0500

    Este es un ejercicio que se hizo en una entrevista de trabajo para el puesto de ReactJS developer.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  Condition = (number) => {
+    if (number % 3 === 0 && number % 5 === 0) {
+      return '#2ecc71';
+    } else if (number % 5 === 0) {
+      return '#00cec9';
+    } else if (number % 3 === 0) {
+      return '#ee5253';
+    } else {
+      return '#f5f6fa';
+    }
+  }
+
   render() {
+    var numbers = Array.apply(null, {length: 101}).map(function(value, index){
+      return index;
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h4 className="Title">Display a list of numbers from 0 to 100</h4>
+        <p className="Instructions-Title"><i>Each list item will have a background of a certain color:</i></p>
+        <ul className="List">
+          <li>For numbers divisible by 3, make the background red,</li>
+          <li>For numbers divisible by 5 make the background blue,</li>
+          <li>For numbers divisible by both 5 and 3, make the background green</li>
+          <li>For numbers not divisible, leave the background white</li>
+        </ul>
+        {
+          numbers.map((number, index) => {
+            return (
+              <p className="Numbers" key={index}
+                style={{ backgroundColor: this.Condition(number)}}
+              >{number}</p>
+            )
+          })
+        }
       </div>
     );
   }
